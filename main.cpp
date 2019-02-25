7b35ee274fad51f6cabc9de567942aaf28f3150b
chaselange <47833138+chaselange@users.noreply.github.com>
2019-02-25 16:23:58 -0600

    Added to test

--- main.cpp
+++ main.cpp
@@ -6,7 +6,17 @@ bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 int main()
 {
     char result[4];
-    multiply(1,0,0,2,0,0,result,4);
+    if(multiply(1,0,0,2,0,0,result,4)==true)
+    {
+        for(int i =0; i < 3; i++) //avoid /0 being printed
+        {
+            cout<<result[i]<<endl;
+        }
+    }
+    else
+    {
+        cout<<"Error trying to multiply values together"<<endl;
+    }
     return 0;
 }
 
