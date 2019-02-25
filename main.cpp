6b06f0e317473ed21d8e687ae88ee0de26b77089
chaselange <47833138+chaselange@users.noreply.github.com>
2019-02-25 16:21:05 -0600

    Added multiply
    Added test for multiply and beginning of function

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
+bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 int main()
 {
+    char result[4];
+    multiply(1,0,0,2,0,0,result,4);
     return 0;
 }
 
 //new functions go here
+bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+{
+
+}
