e1e6bc33b80742ef6e2b7d8d878fbe6f8c9cb9c2
chaselange <47833138+chaselange@users.noreply.github.com>
2019-02-25 16:36:32 -0600

    Added another test

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 int main()
 {
     char result[4];
-    if(multiply(1,0,0,2,0,0,result,4)==true)
+    if(multiply(1,0,0,2,0,0,result,4)==true) //test with "whole numbers"
     {
         for(int i =0; i < 3; i++) //avoid /0 being printed
         {
@@ -17,11 +17,25 @@ int main()
     {
         cout<<"Error trying to multiply values together"<<endl;
     }
+    if(multiply(1,1,2,2,1,2,result,4)==true) //test with fractions should result in 3.75
+    {
+        for(int i =0; i < 3; i++) //avoid /0 being printed
+        {
+            cout<<result[i]<<endl;
+        }
+    }
+    else
+    {
+        cout<<"Error trying to multiply values together"<<endl;
+    }
+
     return 0;
 }
 
 //new functions go here
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
+    bool retval = false; //assume there will be errors
 
+    return retval;
 }
