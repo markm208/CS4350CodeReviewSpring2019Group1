bff073c7a3e7547d0270299464302ddf54ab257a
chaselange <47833138+chaselange@users.noreply.github.com>
2019-02-27 09:36:40 -0600

    Added working values

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 int main()
 {
-    char result[4];
-    if(multiply(1,0,0,2,0,0,result,4)==true) //test with "whole numbers"
+    char result[9];
+    if(multiply(1,0,0,2,0,0,result,9)==true) //test with "whole numbers"
     {
-        for(int i =0; i < 3; i++) //avoid /0 being printed
+        for(int i =0; i < 4; i++) //avoid /0 being printed
         {
             cout<<result[i]<<endl;
         }
@@ -36,6 +36,11 @@ int main()
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
     bool retval = false; //assume there will be errors
+    int newNumeratorOne = c1 + n1; //add the whole number to the numerator for first number
+    int newNumeratorTwo = c2+n2; //add the whole to the numerator for second number
+    int multipliedNumerators = newNumeratorOne * newNumeratorTwo; //multiply the numerators together
+    int multipliedDenominators = d1 * d2; //no converting needs to be done to the denominators so they are multiplied together
+    //now left with imperfect fraction 
 
     return retval;
 }
