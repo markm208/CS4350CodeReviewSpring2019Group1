3009d3225f6f08abcbee3b8f2b671bacd354173d
chaselange <47833138+chaselange@users.noreply.github.com>
2019-02-27 12:16:59 -0600

    Fixed error in calculating numerators for multiplication

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main()
     }
     if(multiply(1,1,2,2,1,2,result,4)==true) //test with fractions should result in 3.75
     {
-        for(int i =0; i < 3; i++) //avoid /0 being printed
+        for(int i =0; i < 9; i++) //avoid /0 being printed
         {
             cout<<result[i]<<endl;
         }
@@ -37,8 +37,9 @@ bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 {
     bool retval = false; //assume there will be errors
     result[len -1] ='/0'; // place null terminated char at end of result so its not left off by mistake
-    int newNumeratorOne = c1 + n1; //add the whole number to the numerator for first number
-    int newNumeratorTwo = c2+n2; //add the whole to the numerator for second number
+    int newNumeratorOne = (c1 * d1) + n1; //must convert to mixed number so multiply whole number by denominator
+                                         //then add the result to the numerator
+    int newNumeratorTwo = (c2*d2)+n2; //repeat above for second fraction
     int multipliedNumerators = newNumeratorOne * newNumeratorTwo; //multiply the numerators together
     int multipliedDenominators = d1 * d2; //no converting needs to be done to the denominators so they are multiplied together
     //now left with improper fraction which must be converted to decimal
