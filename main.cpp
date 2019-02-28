d368bd3ace6b3213483b82e0760a264694929551
Gavin Kaepernick <kaepernick.gavin6@gmail.com>
2019-02-27 20:15:53 -0600

    Added in debugging code

--- main.cpp
+++ main.cpp
@@ -4,13 +4,22 @@ using namespace std;
 
 int main()
 {
+    // Number A      = 21.24;
+    // Number B      = 4.63;
+    // Number Answer = 16.61
+
+    char answer[10];
+    int c1, n1, d1;
+    int c2, n2, d2;
+
+
 
     return 0;
 }
 
 //new functions go here
 
-bool subtract() 
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len) 
 {
-    cout << "Hello World" << endl;
+
 }
