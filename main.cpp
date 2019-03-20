5c61171cc0b7dfb6e1605c76c61ecbe294160caf
chaselange <47833138+chaselange@users.noreply.github.com>
2019-03-20 15:33:56 -0500

    Fix in VS

--- main.cpp
+++ main.cpp
@@ -5,44 +5,75 @@ using namespace std;
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 char *convertToChars(int toBeConverted, int len);
 char *getDecimalPortion(int dividedInto, int dividedBy, int len);
+void testMultiply();
 int main()
-{
-	char result[10];
-	if (multiply(1, 0, 0, 2, 0, 0, result, 10) == true) //test with "whole numbers"
-	{
-		for (int i = 0; i < 4; i++) //avoid /0 being printed
-		{
-			cout << static_cast<int>(result[i]); //have to cast back to an int to get proper numbers
-												 //anything over 255 cast as a char will result in wrong values being printed otherwise
-		}
-		cout << endl;
-	}
-	else
-	{
-		cout << "Error trying to multiply values together" << endl;
-	}
-	if (multiply(1, 1, 2, 2, 1, 2, result, 10) == true) //test with fractions should result in 3.75
-	{
-		for (int i = 0; i < 9; i++) //avoid /0 being printed
-		{
-			cout << static_cast<int>(result[i]);
-		}
-		cout << endl;
-	}
-	else
-	{
-		cout << "Error trying to multiply values together" << endl;
-	}
+{	
+
+	//math function tests
+	testMultiply();
+
 
 	system("pause");
 	return 0;
 }
+void testMultiply()
+{
+	const int SHORT_ARRAY_LENGTH = 5;
+	char shortArray[SHORT_ARRAY_LENGTH];
 
+	const int MEDIUM_ARRAY_LENGTH = 10;
+	char mediumArray[MEDIUM_ARRAY_LENGTH];
+
+	const int LARGE_ARRAY_LENGTH = 20;
+	char largeArray[LARGE_ARRAY_LENGTH];
+
+	//should not be enough space in the array for the result
+	if (multiply(INT_MAX, 0, 10, INT_MAX, 0, 10, shortArray, SHORT_ARRAY_LENGTH))
+	{
+		cout << "Error: not enough space in array" << endl;
+	}
+
+	//0 * 0 = "0"
+	multiply(0, 0, 10, 0, 0, 10, shortArray, SHORT_ARRAY_LENGTH);
+	multiply(0, 0, 10, 0, 0, 10, mediumArray, MEDIUM_ARRAY_LENGTH);
+	multiply(0, 0, 10, 0, 0, 10, largeArray, LARGE_ARRAY_LENGTH);
+
+	//3 * 2 = "6"
+	multiply(3, 0, 10, 2, 0, 10, shortArray, SHORT_ARRAY_LENGTH);
+	multiply(3, 0, 10, 2, 0, 10, mediumArray, MEDIUM_ARRAY_LENGTH);
+	multiply(3, 0, 10, 2, 0, 10, largeArray, LARGE_ARRAY_LENGTH);
+
+
+	//3 * -1.5 = "-4.5"
+	multiply(3, 0, 10, -1, 1, 2, shortArray, SHORT_ARRAY_LENGTH);
+	multiply(3, 0, 10, -1, 1, 2, mediumArray, MEDIUM_ARRAY_LENGTH);
+	multiply(3, 0, 10, -1, 1, 2, largeArray, LARGE_ARRAY_LENGTH);
+
+	//1.125 * 1.6R = "1.87"
+	multiply(1, 1, 8, 1, 2, 3, shortArray, SHORT_ARRAY_LENGTH);
+
+	//1.125 * 1.6R = "1.875"
+	multiply(1, 1, 8, 1, 2, 3, mediumArray, MEDIUM_ARRAY_LENGTH);
+
+	//1.125 * 1.6R = "1.875"
+	multiply(1, 1, 8, 1, 2, 3, largeArray, LARGE_ARRAY_LENGTH);
+	char expectedResult[] = "1.875";
+	for (int i = 0; i < LARGE_ARRAY_LENGTH; i++)
+	{
+		if (expectedResult[i] != largeArray[i])
+		{
+			cout << "Error: mismatch in C strings in multiply()." << endl
+				<< "Expected: " << expectedResult << " "
+				<< "Actual: " << largeArray
+				<< endl;
+		}
+	}
+}
 //new functions go here
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
-	bool retval = false;	 //assume there will be errors
-	result[len - 1] = '\0';  // place null terminated char at end of result so its not left off by mistake
+	bool retval = false;	//assume there will be errors
+	result[len - 1] = '\0'; // place null terminated char at end of result so its not left off by mistake
 	bool isNegative = false; //assume both are positive
 	if (c1 < 0 && c2 >= 0 || c1 >= 0 && c2 < 0)
 	{
@@ -81,27 +112,29 @@ bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 
 	//we now must get the character array of the whole number and put it into our results
 	char *toChar = convertToChars(newWholeNum, len);
-	int pos = 0; //start at beginning of toChar array
+	int pos = 0;//start at beginning of toChar array
+	int i = 0; //will be used to move through result array
 	//however if we have a negative we must insert a negative sign at the beginning instead
 	if (isNegative == true)
 	{
-		//pos now begins at 1
-		pos = 1;
+		//i now begins at 1
+		i = 1;
 		//first member of result is now a negative
-		result[0] = '-';
+		result[0] = '-'; 
+
 	}
 
 	//this loop checks for leading zeroes and makes sure none are picked up as part of the answer
-	while (toChar[pos] == 0)
+	while (toChar[pos] == '0')
 	{
 		pos++; //increment until we hit something that isn't a zeros
 	}
 
 	//this loop moves through the result array from the beginning and inserts the answer from toChar
 	//this will remove the need for leading zeroes
-	int i = 0; //will be used to move through result array
 	while (pos < len && i < len)
 	{
+
 		result[i] = toChar[pos]; //this will put the number in the correct order at the front of the result array
 		pos++;
 		i++;
@@ -126,8 +159,10 @@ bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 		{
 			retval = true; //if we got to here its safe to assume we were able to insert things without issue and can set-
 						   //-retval to true
+			break;
 		}
 	}
+
 
 	i = 0;   //reset i just to make sure next pass through function has clean slate
 	pos = 0; //reset pos just to make sure next pass through function has clean slate
@@ -143,20 +178,20 @@ char *convertToChars(int toBeConverted, int len)
 	char *arr = new char[len]; //array to hold values, will be in reverse order
 	for (int i = 0; i < len - 1; i++)
 	{
-		arr[i] = static_cast<char>(0); //fill array with zeroes for easier parsing later
+		arr[i] = '0' + (0); //fill array with zeroes for easier parsing later
 	}
 	arr[len - 1] = '\0';
 	int count = len - 2; //start from two less then len to avoid overwriting null char
 	while (toBeConverted > 0)
 	{
-		arr[count] = static_cast<char>(toBeConverted % 10);
+		arr[count] = '0' + (toBeConverted % 10);
 		count--; //putting actual number at end makes it easier to remove zeroes later on
 		toBeConverted = toBeConverted / 10;
 	}
 	return arr;
 }
 char *getDecimalPortion(int dividedInto, int dividedBy, int len) //newfunction to get the decimal portion of float
-{																 //dividedInto divided by dividedBy
+{
 	char *arr = new char[len];
 	arr[0] = static_cast<char>(dividedBy); //assign first position in array to be first remainder
 	int counter = 1;
@@ -164,7 +199,7 @@ char *getDecimalPortion(int dividedInto, int dividedBy, int len) //newfunction t
 	{
 		while (dividedInto % dividedBy > 1 && counter < len) //keep track of remainder from mod so we are not looping unnecessarily
 		{
-			arr[counter] = static_cast<char>(dividedInto % dividedBy);
+			arr[counter] = '0' + (dividedInto % dividedBy);
 			dividedInto = dividedBy;
 			dividedBy = arr[counter];
 			counter++;
