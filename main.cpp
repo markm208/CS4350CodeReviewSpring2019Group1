bca52d18f8264896ac5a585ccda9cf574fcb8c1b
aheiting <aheiting@carthage.edu>
2019-03-22 15:42:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ void shouldConvert(char number[], int expectedCharacteristic, int expectedNumera
 void shouldNotConvert(char number[]);
 
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 int power(int x, int y);
 
@@ -500,6 +501,69 @@ bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 	}
 
 	return retVal;
+}
+
+
+
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len) 
+{
+    // Turns the numbers into complex fractions
+    int numberOne = (c1 * d1) + n1;
+    int numberTwo = (c2 * d2) + n2;
+
+    // Making common denominators
+    int commonDenom;
+    if (d1 != d2) 
+    {
+        numberOne = numberOne * d2;
+        numberTwo = numberTwo * d1;
+        commonDenom = d1 * d2;
+    }
+    
+    // Subtract newly complex numbers
+    int subtractedNum = numberOne - numberTwo;
+    int newCharacteristic = subtractedNum / commonDenom;
+    //cout << newCharacteristic << endl;
+    
+    // Calculating how many digets the characteristic has in it
+    int digetcounter;
+    int temporaryNumber;
+    while (temporaryNumber != 0)
+    {
+        digetcounter++;
+        temporaryNumber = temporaryNumber / 10;
+    }
+    if (digetcounter > len)
+    {
+        return false;
+    }
+    else
+    {
+        // For if the number is less than 1 but greater than 0
+        if (digetcounter == 0)
+        {
+            result[0] = '.';
+            digetcounter++;
+        }
+        else
+        {
+            for (int i = 0; i < digetcounter; i++)
+            {
+                //  Trying to place the numbers inside the char array.
+                int r = temporaryNumber % 10;
+                result[i + 1] = char('0' + r);
+                temporaryNumber = temporaryNumber / 10;
+            }
+            digetcounter++;
+        }
+        
+    }
+    //cout << digetcounter << endl;
+    //  Trying to work back out of the complex fraction
+    int remaindingDec = (newCharacteristic * digetcounter) / commonDenom;
+    //cout << remaindingDec << endl;
+    
+   return true;
 }
 
 
